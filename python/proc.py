a088018ae7eccf0d59f998a8935830f54e18fe74
Josh Gulden <jdg78@pitt.edu>
2016-02-20 16:42:26 -0500

    Added 2 range methods

--- python/proc.py
+++ python/proc.py
@@ -3,7 +3,7 @@
 def procData(data):
     for el in data: #split by readings
         el.strip('\n').split(',') #get separated data
-        
+
 
 #take file, make array of each timestamped (line number) data value
 def procFile(fileNm):
@@ -12,6 +12,31 @@ def procFile(fileNm):
     for line in f:
         data.append(line)
     return data
+
+def checkRangeShoulderUpDown(myoUpDown):
+    maxRange = 90
+    minRange = -90
+
+    if myoUpDown < maxRange and myoUpDown > minRange:
+        inRange = true
+    else:
+        inRange = false
+
+    return inRange
+
+def checkRangeWristTwist(myoWristTwist):
+    maxRange = 80
+    minRange = -80
+
+    if myoWristTwist < maxRange and myoWristTwist > minRange:
+        inRange = true
+    else:
+        inRange = false
+
+    return inRange
+
+## def checkRangeLeftRight(myoUpDown, myoWristTwist):
+
 
 def main():
     data = procFile("data.txt")
