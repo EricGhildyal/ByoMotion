927e7aaeedf3aabe2c4c27739922b69a64dc5ff3
Alex Dawson <akd40@pitt.edu>
2016-02-22 14:07:22 -0500

    An unfinished/unimplemented prototype of the GUI that we ran out of time before I could finish

---
+++ Unimplemented GUI Proto/Unimplemented.GUI.Proto.py
@@ -0,0 +1,14 @@
+from PIL import Image
+
+image = Image.open('1.png')
+image.show()
+image.close();
+
+image = Image.open('2.png')
+image.show()
+image.close()
+
+image = Image.open('3.png')
+image.show()
+image.close()
+
